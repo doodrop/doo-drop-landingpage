2cd9c687b08ea09f8b2a62c2d513ef189669d6e6
Richard Siwady <rsiwady29@gmail.com>
2016-08-05 16:29:50 -0600

    Fixed error/success messages logic. Added loading spinner. Added styling for messages(loading,success, error) area

--- js/mail.js
+++ js/mail.js
@@ -2,26 +2,35 @@
 $(document).ready(function() {
 	$("#subscribe-form").submit(function(e){
 		e.preventDefault();
+		$('#loading-spinner').show();
+
 		var model={
-		    "email":$("#email").val(),
-		    "firstName":$("#first-name").val(),
-		    "lastName": $("#last-name").val()
+			"email": $("#email").val(),
+			"firstName": $("#first-name").val(),
+			"lastName": $("#last-name").val()
 		};
 
 		$.ajax({
-		    type: "POST",
-		    url: "https://ancient-crag-99619.herokuapp.com/send-email",
-		    data: JSON.stringify(model),
-		    contentType: "application/json; charset=utf-8",
-		    success: function(msg) {
-				$('#success-message').css('display','block');
-				console.log('#success-message');	
-		    },
-		    error: function(error) {
-		       $('#error-message').css('display','block')
-	    	}
-   		});
-   		return false;
+			type: "POST",
+			url: "https://ancient-crag-99619.herokuapp.com/send-email",
+			data: JSON.stringify(model),
+			contentType: "application/json; charset=utf-8",
+			success: function(msg) {
+				$('#success-message').show();
+				$('#error-message').hide();
+				$('#loading-spinner').hide();
+			},
+			error: function(error) {
+				$('#error-message').show();
+				$('#success-message').hide();
+				$('#loading-spinner').hide();
+			}
+		});
+
+		$("#email").val('');
+		$("#first-name").val('');
+		$("#last-name").val('');
+		return false;
 	});
 
 });
